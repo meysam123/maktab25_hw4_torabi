64633d545a58ef8264d2198327d82221f4750194
unknown <hamid.r_torabi@yahoo.com>
2019-03-12 21:05:21 +0330

    Adds Scanner and some checkers for Entered data

--- Main3.java
+++ Main3.java
@@ -1,10 +1,31 @@
 package com.maktab25.hw4.torabi.practice3;
 
-public class Main3 {
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println(Billing.computeBill("p"));;
-        System.out.println(Billing.computeBill("p",2));;
-        Billing.setPhotoBookPrice(154);
-        System.out.println(Billing.computeBill("p",2,100));;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Price of a Photo Book");
+        int price = Integer.parseInt(scanner.nextLine());
+        Billing.setPhotoBookPrice(price);
+        System.out.println("Enter name of Photo Book");
+        String photoBook = scanner.nextLine();
+        System.out.println("Enter number of the Photo Books");
+        String quantityChecker = scanner.nextLine();
+        int quantity=0;
+        if (!quantityChecker.isEmpty())
+            quantity = Integer.parseInt(quantityChecker);
+        System.out.println("Enter Coupon Value:if you don't have any,press Enter");
+        String couponChecker = scanner.nextLine();
+        int couponValue=0;
+        if (!couponChecker.isEmpty())
+            couponValue = Integer.parseInt(couponChecker);
+        if (quantityChecker.isEmpty())
+            Billing.computeBill(photoBook);
+        else if (couponChecker.isEmpty())
+            Billing.computeBill(photoBook, quantity);
+
+        else
+            Billing.computeBill(photoBook, quantity, couponValue);
     }
 }
