6250ec38b9a097570d5a1848e0269036a6b43db8
unknown <hamid.r_torabi@yahoo.com>
2019-03-12 11:32:20 +0330

    Put a Main class file contains main

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.maktab25.hw4.torabi.practice2;
+
+public class Main {
+    public static void main(String[] args) {
+        FormLetterWriter.displaySalutation("hamid","torabi");
+        FormLetterWriter.displaySalutation("torabi");
+        FormLetterWriter.displaySalutation("");
+    }
+}
