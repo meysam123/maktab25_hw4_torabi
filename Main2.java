f18c19e6a5b247df9967e4d698386ef6381c5ef1
HTorabi <48438920+HTorabi@users.noreply.github.com>
2019-03-12 15:44:20 +0330

    update and rename Main to Main2

--- Main2.java
+++ Main2.java
@@ -1,6 +1,6 @@
 package com.maktab25.hw4.torabi.practice2;
 
-public class Main {
+public class Main2 {
     public static void main(String[] args) {
         FormLetterWriter.displaySalutation("hamid","torabi");
         FormLetterWriter.displaySalutation("torabi");
