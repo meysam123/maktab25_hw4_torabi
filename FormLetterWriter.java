fcdf096bb2b9791cc1bd500a63c89337c887e9e2
unknown <hamid.r_torabi@yahoo.com>
2019-03-12 11:29:52 +0330

    Adds two method displaySalutation with one and two args

--- FormLetterWriter.java
+++ FormLetterWriter.java
@@ -2,5 +2,15 @@ package com.maktab25.hw4.torabi.practice2;
 
 public class FormLetterWriter {
 
+    static void displaySalutation(String lastName) {
+        System.out.println("Dear Mr. or Ms. " + lastName);
+        System.out.println("  Thank you for your recent order"+"\n");
 
+    }
+
+    static void displaySalutation(String firstName, String lastName) {
+        System.out.println("Dear " + firstName + " " + lastName);
+        System.out.println("  Thank you for your recent order"+"\n");
+
+    }
 }
