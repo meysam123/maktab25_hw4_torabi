815da2a5852b945c3942c279d588cd7dbde518ca
unknown <hamid.r_torabi@yahoo.com>
2019-03-12 16:38:59 +0330

    Fixes BloodData parameters in TesBloodData class

--- TestBloodData.java
+++ TestBloodData.java
@@ -2,7 +2,7 @@ package com.maktab25.hw4.torabi.practice4;
 
 public class TestBloodData {
     public static void main(String[] args) {
-        BloodData bloodData=new BloodData("ab",'-');
+        BloodData bloodData=new BloodData("AB",'-');
         System.out.println(bloodData.getBloodType()+bloodData.getFactor());
     }
 }
