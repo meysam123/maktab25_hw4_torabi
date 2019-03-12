9158f30248d2ac59d633d25beb7030b28903719a
unknown <hamid.r_torabi@yahoo.com>
2019-03-12 15:02:54 +0330

    Adds class attributes and their setter/getter

--- Patient.java
+++ Patient.java
@@ -1,5 +1,13 @@
 package com.maktab25.hw4.torabi.practice4;
 
 public class Patient {
+    private int id;
+    private int age;
+    public int getAge() {
+        return age;
+    }
 
+    public int getId() {
+        return id;
+    }
 }
