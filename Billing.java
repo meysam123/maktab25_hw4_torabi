332804b848b38fcc7d3a7894cc9b91246b8bd165
unknown <hamid.r_torabi@yahoo.com>
2019-03-12 20:59:36 +0330

    Changes some user interfaces in printer

--- Billing.java
+++ Billing.java
@@ -1,30 +1,32 @@
 package com.maktab25.hw4.torabi.practice3;
 
 public class Billing {
-    private static int photoBookPrice = 150;
+    private static int photoBookPrice;
 
     public static void setPhotoBookPrice(int photoBookPrice) {
-        Billing.photoBookPrice = photoBookPrice;
+        com.maktab25.hw4.torabi.practice3.Billing.photoBookPrice = photoBookPrice;
     }
 
     public int getPhotoBookPrice() {
         return photoBookPrice;
     }
-    static double computeBill(String photoBook) {
-        System.out.println("price of a Photo Book:" + photoBookPrice);
-        return photoBookPrice * (1 + 0.08);
+
+    static void computeBill(String photoBook) {
+        System.out.println("price of "+photoBook+" is: " + photoBookPrice);
+        System.out.println("total price:"+photoBookPrice * (1 + 0.08));
+
 
     }
 
-    static double computeBill(String photoBook, int quantity) {
-        System.out.println("price of a Photo Book:" + photoBookPrice + "\n" + "quantity:" + quantity);
-        return photoBookPrice * quantity * (1 + 0.08);
-
+    static void computeBill(String photoBook, int quantity) {
+        System.out.println("price of "+photoBook+" is:" + photoBookPrice + "\n" + "quantity:" + quantity);
+        System.out.println("total price:"+photoBookPrice * quantity * (1 + 0.08));
     }
 
-    static double computeBill(String photoBook, int quantity, int couponValue) {
-        System.out.println("price of a Photo Book:" + photoBookPrice + "\n" + "quantity:" + quantity + "\n" + "coupon value:" + couponValue);
-        return (photoBookPrice * quantity-couponValue)*1.08;
+    static void computeBill(String photoBook, int quantity, int couponValue) {
+        System.out.println("price of "+photoBook+" is:" + photoBookPrice + "\n" + "quantity:" + quantity + "\n" + "coupon value:" + couponValue);
+        System.out.println("total price:"+ (photoBookPrice * quantity-couponValue)*1.08);
+
 
     }
 }
