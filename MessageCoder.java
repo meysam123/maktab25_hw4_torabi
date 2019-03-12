f79d833b4205cbfea8819b52d7353b859c1188ef
unknown <hamid.r_torabi@yahoo.com>
2019-03-12 11:08:40 +0330

    adds its attributes and setter/getter methods

--- MessageCoder.java
+++ MessageCoder.java
@@ -1,4 +1,20 @@
 package com.maktab25.hw4.torabi.practice1;
 public class MessageCoder {
+    private String message;
+    private int code;
+    public void setCode(int code) {
+        this.code = code;
+    }
 
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    private String getMessage() {
+        return message;
+    }
+
+    public int getCode() {
+        return code;
+    }
 }
