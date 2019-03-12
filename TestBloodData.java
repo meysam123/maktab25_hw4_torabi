7cc1c470c877dbfb2d953f01f7f02a372301a16a
unknown <hamid.r_torabi@yahoo.com>
2019-03-12 16:36:05 +0330

    Fixes println

--- TestBloodData.java
+++ TestBloodData.java
@@ -3,6 +3,6 @@ package com.maktab25.hw4.torabi.practice4;
 public class TestBloodData {
     public static void main(String[] args) {
         BloodData bloodData=new BloodData("ab",'-');
-        System.out.println(bloodData.getBloodType());
+        System.out.println(bloodData.getBloodType()+bloodData.getFactor());
     }
 }
