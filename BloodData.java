4dc832690f7a62e189315a0bcd4ed01dd1e3c057
unknown <hamid.r_torabi@yahoo.com>
2019-03-12 14:39:47 +0330

    Adds it attributs and setter/getter for them

--- BloodData.java
+++ BloodData.java
@@ -1,5 +1,21 @@
 package com.maktab25.hw4.torabi.practice4;
 
 public class BloodData {
+    private String bloodType;
+    private char factor;
+    public void setBloodType(String bloodType) {
+        this.bloodType = bloodType;
+    }
 
+    public void setFactor(char factor) {
+        this.factor = factor;
+    }
+
+    public char getFactor() {
+        return factor;
+    }
+
+    public String getBloodType() {
+        return bloodType;
+    }
 }
