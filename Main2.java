f63df71c789915b78eef58ab7154c3559066a610
unknown <hamid.r_torabi@yahoo.com>
2019-03-12 18:37:08 +0330

    Adds scanner for receiving first name and last name

--- Main2.java
+++ Main2.java
@@ -1,9 +1,17 @@
 package com.maktab25.hw4.torabi.practice2;
 
-public class Main2 {
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        FormLetterWriter.displaySalutation("hamid","torabi");
-        FormLetterWriter.displaySalutation("torabi");
-        FormLetterWriter.displaySalutation("");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your Last Name");
+        String lastName = scanner.nextLine();
+        System.out.println("Enter your First Name");
+        String firstName = scanner.nextLine();
+        if (firstName.isEmpty())
+            FormLetterWriter.displaySalutation(lastName);
+        else
+            FormLetterWriter.displaySalutation(firstName, lastName);
     }
 }
