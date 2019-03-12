b438f30a9e204abb9dc9f56b9ee14128daea10b5
HTorabi <48438920+HTorabi@users.noreply.github.com>
2019-03-12 15:42:06 +0330

    Fixes class name

--- Main1.java
+++ Main1.java
@@ -1,6 +1,6 @@
 package com.maktab25.hw4.torabi.practice1;
 
-public class Main {
+public class Main1 {
     public static void main(String[] args) {
         MessageCoder messageCoder = new MessageCoder("THE REDCOATS ARE COMING! ", +5);
         System.out.println(messageCoder.encryptAll());
