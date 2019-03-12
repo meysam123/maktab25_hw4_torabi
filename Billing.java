70ead305a68ce4b66896967e9f22e37b625f8eb5
unknown <hamid.r_torabi@yahoo.com>
2019-03-12 11:54:58 +0330

    Put an attribute and setter/getter

--- Billing.java
+++ Billing.java
@@ -1,5 +1,13 @@
 package com.maktab25.hw4.torabi.practice3;
 
 public class Billing {
+    private static int photoBookPrice = 150;
 
+    public static void setPhotoBookPrice(int photoBookPrice) {
+        Billing.photoBookPrice = photoBookPrice;
+    }
+
+    public int getPhotoBookPrice() {
+        return photoBookPrice;
+    }
 }
