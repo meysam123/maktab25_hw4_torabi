0ef0a8c06598208c0f097fc46ad0a161aae60e00
unknown <hamid.r_torabi@yahoo.com>
2019-03-12 14:44:04 +0330

    Create main class:TestBloodData fot testing BloodData on objects

---
+++ TestBloodData.java
@@ -0,0 +1,8 @@
+package com.maktab25.hw4.torabi.practice4;
+
+public class TestBloodData {
+    public static void main(String[] args) {
+        BloodData bloodData=new BloodData("ab",'-');
+        System.out.println(bloodData.getBloodType());
+    }
+}
