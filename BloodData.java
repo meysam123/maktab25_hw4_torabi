ae5e9ad09bebbcc6afefcdf05d7dcd0acc89f2a5
unknown <hamid.r_torabi@yahoo.com>
2019-03-13 08:34:25 +0330

    Adds Conditions for blood type and factor to BloodData class

--- BloodData.java
+++ BloodData.java
@@ -3,6 +3,24 @@ package com.maktab25.hw4.torabi.practice4;
 public class BloodData {
     private String bloodType;
     private char factor;
+
+    BloodData() {
+        bloodType = "O";
+        factor = '+';
+    }
+
+    BloodData(String bloodType, char factor) {
+        if (bloodType.equalsIgnoreCase("AB") || bloodType.equalsIgnoreCase("A") || bloodType.equalsIgnoreCase("B") || bloodType.equalsIgnoreCase("O"))
+            this.bloodType = bloodType.toUpperCase();
+        else
+            System.out.println("This Blood Type is not valid");
+        if (factor == '-' || factor == '+')
+            this.factor = factor;
+        else
+            System.out.println("This factor is not valid");
+
+    }
+
     public void setBloodType(String bloodType) {
         this.bloodType = bloodType;
     }
@@ -18,13 +36,4 @@ public class BloodData {
     public String getBloodType() {
         return bloodType;
     }
-    BloodData() {
-        bloodType = "O";
-        factor = '+';
-    }
-    BloodData(String bloodType, char factor) {
-            this.bloodType = bloodType.toUpperCase();
-            this.factor = factor;
-    }
-
 }
