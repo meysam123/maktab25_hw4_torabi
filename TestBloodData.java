0d420f63e857ca77ada5966ad01c82050fc68711
unknown <hamid.r_torabi@yahoo.com>
2019-03-13 08:35:50 +0330

    Remove conditions from main method and put another condition

--- TestBloodData.java
+++ TestBloodData.java
@@ -1,20 +1,18 @@
 package com.maktab25.hw4.torabi.practice4;
+
 import java.util.Scanner;
 
 public class TestBloodData {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter Blood Type");
-        String bloodType=scanner.nextLine();
+        String bloodType = scanner.nextLine();
         System.out.println("Enter factor:+ or -");
-        char factor=scanner.nextLine().charAt(0);
+        char factor = scanner.nextLine().charAt(0);
         BloodData bloodData = new BloodData(bloodType, factor);
-        if (bloodData.getBloodType().equalsIgnoreCase("AB") || bloodData.getBloodType().equalsIgnoreCase("A") || bloodData.getBloodType().equalsIgnoreCase("B") || bloodData.getBloodType().equalsIgnoreCase("O"))
-            if (bloodData.getFactor() == '-' || bloodData.getFactor() == '+')
-                System.out.println(bloodData.getBloodType() + bloodData.getFactor());
-            else
-                System.out.println("This factor is not valid");
-        else
-            System.out.println("This Blood Type is not valid");
+        if (bloodData.getBloodType() != null && (bloodData.getFactor() == '+' || bloodData.getFactor() == '-'))
+
+            System.out.println(bloodData.getBloodType() + bloodData.getFactor());
+
     }
 }
