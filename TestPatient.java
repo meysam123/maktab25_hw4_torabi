2dd0903718fcea2d77f531e53988f1bbddf7d33a
unknown <hamid.r_torabi@yahoo.com>
2019-03-13 08:42:18 +0330

    Adds Scanner and a condition when blood type is not valid

--- TestPatient.java
+++ TestPatient.java
@@ -1,8 +1,24 @@
 package com.maktab25.hw4.torabi.practice4;
 
+import java.util.Scanner;
+
 public class TestPatient {
     public static void main(String[] args) {
-        Patient patient=new Patient(1,25,new BloodData("ab",'+'));
-        System.out.println(patient.getBloodData().getFactor());;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter ID");
+        int id = scanner.nextInt();
+        System.out.println("Enter age");
+        int age = scanner.nextInt();
+        System.out.println("Enter Blood type and factor a space between them");
+        String bloodType = scanner.next();
+        char factor = scanner.next().charAt(0);
+        Patient patient = new Patient(id, age, new BloodData(bloodType, factor));
+        if (patient.getBloodData().getBloodType() != null && (patient.getBloodData().getFactor() == '+' || patient.getBloodData().getFactor() == '-')) {
+            System.out.println("ID:"+id);
+            System.out.println("Age:"+age);
+            System.out.println(patient.getBloodData().getBloodType() + patient.getBloodData().getFactor());
+        }
+
+
     }
 }
