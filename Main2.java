ddbadcb5ab9eeaef9364b54601a07334cc4b6737
unknown <hamid.r_torabi@yahoo.com>
2019-03-13 12:19:19 +0330

    Change method name

--- Main2.java
+++ Main2.java
@@ -2,7 +2,7 @@ package com.maktab25.hw4.torabi.practice2;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your Last Name");
